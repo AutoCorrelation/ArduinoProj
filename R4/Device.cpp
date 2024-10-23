7e48185aef1596b9b60d06d48d3d7d8d7d2e69d5
AutoCorrelation <minjun.chung@gmail.com>
2024-10-23 20:16:54 +0900

    few changes

--- R4/Device.cpp
+++ R4/Device.cpp
@@ -1,59 +1,71 @@
 #include "Device.h"
 
-Device::Device() : O2(), CO2()
-{
-    // 초기화 작업
+Device::Device() : O2(), CO2() {
     _x1 = _P1 = 0.0;
     _x2 = _P2 = 0.0;
     _x3 = _P3 = 0.0;
 }
 
-Device::~Device()
-{
-
-    // 정리 작업 (필요 시)
+Device::~Device() {
+    // Cleanup if necessary
 }
 
-bool Device::init()
-{
+bool Device::init() {
     bool oxygenSensorInit = O2.begin();
     bool co2SensorInit = CO2.begin();
     return oxygenSensorInit && co2SensorInit;
 }
 
-void Device::fastCalibrate()
-{
+void Device::fastCalibrate() {
     O2.calibrate();
-    CO2.();
+    CO2.enableAutomaticSelfCalibration();
 }
 
-float Device::getRER()
-{
+float Device::getRER() {
     float oxygenConcentration = O2.readOxygenConcentration();
     float co2Concentration = CO2.readCO2Concentration();
     return co2Concentration / oxygenConcentration;
 }
 
-float Device::getMAXRER()
-{
+float Device::getMAXRER() {
     float maxRER = 0.0;
-    for (int i = 0; i < 25; i++)
-    {
+    for (int i = 0; i < 25; i++) {
         float currentRER = getRER();
-        if (currentRER > maxRER)
-        {
+        if (currentRER > maxRER) {
             maxRER = currentRER;
         }
     }
     return maxRER;
 }
 
-void Device::KF_Alg()
-{
-    // 칼만 필터 알고리즘 구현
+void Device::KF_Alg() {
+    // Implement Kalman Filter algorithm
 }
 
-void Device::LPF()
+void Device::LPF() {
+    const float alpha = 0.1;
+    static float filteredValue = 0.0;
+    float currentRER = getRER();
+    filteredValue = alpha * currentRER + (1 - alpha) * filteredValue;
+}
+
+void Device::sleepMode() {
+    CO2.enterSleepMode();
+}
+
+void Device::wakeUp() {
+    
+}
+
+void Device::setDeviceMode(DeviceMode mode)
 {
-    // 저역 필터 알고리즘 구현
+    switch (mode)
+    {
+    case DeviceMode::SLEEP:
+        sleepMode();
+        break;
+    case DeviceMode::WAKEUP:
+        wakeUp();
+        break;
+    }
 }
